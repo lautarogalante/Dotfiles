e1dd45e521bdcae10bc01054cf0442317f990260
lautaro <lautaro9galante@gmail.com>
2021-12-21 22:06:48 -0300

    update

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -4,14 +4,14 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono Bold Italic Nerd Font:size=15" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Caskaydia Cove Nerd Font Complete:size=15" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#614809";		/*"#34606f";  "#744700"; "#005577";*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -95,11 +95,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,			XK_v,	     spawn,	  SHCMD("firefox") },
+	{ MODKEY,			XK_v,	   spawn,	  SHCMD("firefox") },
 	{ MODKEY,			XK_g, 	   spawn,	  SHCMD("rofi -show drun") },
-	{ MODKEY,			XK_o,	     spawn,	  SHCMD("alacritty") },
-  { MODKEY,     XK_z,      spawn,   SHCMD("redshift -O 2400") },
-  { MODKEY,     XK_x,      spawn,   SHCMD("redshift -x") }
+	{ MODKEY,			XK_o,	   spawn,	  SHCMD("alacritty") },
+  	{ MODKEY,     			XK_z,      spawn,   	  SHCMD("redshift -O 2400") },
+  	{ MODKEY,     			XK_x,      spawn,   	  SHCMD("redshift -x") }
 };
 
 /* button definitions */
--- config/dwmblocks/blocks.h
+++ config/dwmblocks/blocks.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"","free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{ "", "date '+%b %d (%a) %H:%M'", 				 	 5,		0},
+	{ "", "date '+%b %d (%a) %H:%M'", 				 5,		0},
 };
   
 /*date '+%b %d (%a) %I:%M%p*/
